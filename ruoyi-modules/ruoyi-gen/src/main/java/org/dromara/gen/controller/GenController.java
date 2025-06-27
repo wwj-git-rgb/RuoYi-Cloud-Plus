77dcf4b0d4dbc89191827b92b3f7d324ee979630
疯狂的狮子Li <15040126243@163.com>
2025-06-27 10:56:09 +0800

    update 优化代码

--- ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/controller/GenController.java
+++ ruoyi-modules/ruoyi-gen/src/main/java/org/dromara/gen/controller/GenController.java
@@ -80,11 +80,8 @@ public class GenController extends BaseController {
     @SaCheckPermission("tool:gen:list")
     @GetMapping(value = "/column/{tableId}")
     public TableDataInfo<GenTableColumn> columnList(@PathVariable("tableId") Long tableId) {
-        TableDataInfo<GenTableColumn> dataInfo = new TableDataInfo<>();
         List<GenTableColumn> list = genTableService.selectGenTableColumnListByTableId(tableId);
-        dataInfo.setRows(list);
-        dataInfo.setTotal(list.size());
-        return dataInfo;
+        return TableDataInfo.build(list);
     }
 
     /**
