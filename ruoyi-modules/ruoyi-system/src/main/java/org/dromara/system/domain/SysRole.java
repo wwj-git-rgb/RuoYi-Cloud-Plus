e878adb6e8bb4fd9d6fcf8d8c166f5dbd7602c13
Lau <devlau@foxmail.com>
2025-08-14 11:12:59 +0000

    !237 update 逻辑删除注释更正改为1 * update 逻辑删除注释更正改为1

--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysRole.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysRole.java
@@ -62,7 +62,7 @@ public class SysRole extends TenantEntity {
     private String status;
 
     /**
-     * 删除标志（0代表存在 2代表删除）
+     * 删除标志（0代表存在 1代表删除）
      */
     @TableLogic
     private String delFlag;
--- ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysTenantPackage.java
+++ ruoyi-modules/ruoyi-system/src/main/java/org/dromara/system/domain/SysTenantPackage.java
@@ -48,7 +48,7 @@ public class SysTenantPackage extends BaseEntity {
      */
     private String status;
     /**
-     * 删除标志（0代表存在 2代表删除）
+     * 删除标志（0代表存在 1代表删除）
      */
     @TableLogic
     private String delFlag;
